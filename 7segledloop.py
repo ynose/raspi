4a3f64f03b5042d045bef279143c23b55e2f9405
ynose <ynose.249@gmail.com>
2015-02-18 22:28:39 +0900

    7セグLEDを2桁に対応

--- 7segledloop.py
+++ 7segledloop.py
@@ -11,17 +11,28 @@ GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)
 
+GPIO.setup(8, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH)
+
 try:
     while True:
         GPIO.output(4,  GPIO.HIGH)  # A
         GPIO.output(17, GPIO.HIGH)  # B
         GPIO.output(27, GPIO.HIGH)  # C
-#        GPIO.output(22, GPIO.HIGH)  # D
-#         GPIO.output(23, GPIO.HIGH)  # E
-#         GPIO.output(24, GPIO.HIGH)  # F
-#         GPIO.output(25, GPIO.HIGH)  # G
-#        GPIO.output(7,  GPIO.HIGH)  # DP
-        sleep(0.5)
+        GPIO.output(23, GPIO.LOW)  # E
+        GPIO.output(24, GPIO.LOW)  # F
+        GPIO.output(8, GPIO.LOW)    # Switch    10-ON
+        GPIO.output(11, GPIO.HIGH)    # Switch
+        sleep(0.01)
+        
+        GPIO.output(4,  GPIO.LOW)  # A
+        GPIO.output(17, GPIO.LOW)  # B
+        GPIO.output(27, GPIO.LOW)  # C
+        GPIO.output(23, GPIO.HIGH)  # E
+        GPIO.output(24, GPIO.HIGH)  # F
+        GPIO.output(8, GPIO.HIGH)   # Switch    1-ON
+        GPIO.output(11, GPIO.LOW)   # Switch
+        sleep(0.01)
 
 except KeyboardInterrupt:
     pass
