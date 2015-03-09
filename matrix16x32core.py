1a5674e0f7ec1585c3c3fa34228abc82e3c3f4bb
ynose_fsd <nose@fsd-jp.com>
2015-03-09 18:07:07 +0900

    Test Diaplay Date

---
+++ matrix16x32core.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import RPi.GPIO as GPIO
+import threading
+import time
+#import smbus
+
+delay = 0.000001
+
+red1_pin = 17
+green1_pin = 18
+blue1_pin = 22
+red2_pin = 23
+green2_pin = 24
+blue2_pin = 25
+clock_pin = 3
+a_pin = 7
+b_pin = 8
+c_pin = 9
+latch_pin = 4
+oe_pin = 2
+
+class MatrixLED:
+    def __init__(self):
+        self.screen = [[0 for x in xrange(32)] for x in xrange(16)]
+#        GPIO.setmode(GPIO.BCM)
+#        GPIO.setwarnings(False)
+#        GPIO.setup(red1_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(green1_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(blue1_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(red2_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(green2_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(blue2_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(clock_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(a_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(b_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(c_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(latch_pin, GPIO.OUT, initial=GPIO.LOW)
+#        GPIO.setup(oe_pin, GPIO.OUT, initial=GPIO.LOW)
+        print "init"
+
+    def screen():
+        return self.sereen
+
+    def bits_from_int(x):
+        a_bit = x & 1
+        b_bit = x & 2
+        c_bit = x & 4
+        return (a_bit, b_bit, c_bit)
+
+    def set_row(row):
+#        #time.sleep(delay)
+        a_bit, b_bit, c_bit = bits_from_int(row)
+#        GPIO.output(a_pin, a_bit)
+#        GPIO.output(b_pin, b_bit)
+#        GPIO.output(c_pin, c_bit)
+#        #time.sleep(delay)
+
+    def set_color_top(color):
+#        #time.sleep(delay)
+        red, green, blue = bits_from_int(color)
+#        GPIO.output(red1_pin, red)
+#        GPIO.output(green1_pin, green)
+#        GPIO.output(blue1_pin, blue)
+#        #time.sleep(delay)
+     
+    def set_color_bottom(color):
+#        #time.sleep(delay)
+        red, green, blue = bits_from_int(color)
+#        GPIO.output(red2_pin, red)
+#        GPIO.output(green2_pin, green)
+#        GPIO.output(blue2_pin, blue)
+#        #time.sleep(delay)
+
+    def clock():
+#        GPIO.output(clock_pin, 1)
+#        GPIO.output(clock_pin, 0)
+        print "clock"
+     
+    def latch():
+#        GPIO.output(latch_pin, 1)
+#        GPIO.output(latch_pin, 0)
+        print "latch"
+
+    def set_pixel(self, x, y, color):
+        self.screen[y][x] = color
+
+    def refresh():
+        # 上下８行ずつに分けて出力する
+        for row in range(8):
+#            GPIO.output(oe_pin, 1)
+            set_color_top(0)
+            set_row(row)
+            #time.sleep(delay)
+            for col in range(32):
+                set_color_top(screen[row][col])
+                set_color_bottom(screen[row+8][col])
+                clock()
+            #GPIO.output(oe_pin, 0)
+            latch()
+#            GPIO.output(oe_pin, 0)
+            time.sleep(delay)
+
+rlock = threading.RLock()
+
+class BitmapNumber:
+    def __init__(self):
+        self.bitmap = [0 for x in xrange(10)]
+
+        self.bitmap[0] = ((0,1,0,0),
+                          (1,0,1,0),
+                          (1,0,1,0),
+                          (1,0,1,0),
+                          (0,1,0,0))
+
+        self.bitmap[1] = ((0,1,0,0),
+                          (1,1,0,0),
+                          (0,1,0,0),
+                          (0,1,0,0),
+                          (1,1,1,0))
+        print "BitmapNumber.init"
+
+    def number(self, no):
+        return self.bitmap[no]
+
+class LedThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.led = MatrixLED()
+        self.dig = [0, 0]
+        self.running = True
+
+    def run(self):
+        while self.running:
+            rlock.acquire()
+            self.led.refresh
+#            time.sleep(0.005)
+            rlock.release
+            
+    def stop(self):
+        self.running = False
+        print "stop"
+
+    def display_date(self, month, day):
+        rlock.acquire()
+        # LEDをOnにする処理を記述
+        bitmap = BitmapNumber()
+
+        # mm.dd
+
+        offset = (0, 11)
+        number = bitmap.number(month)
+        self.led.set_pixel(0,1,1)
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        offset = (4, 11)
+        number = bitmap.number(month)
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        offset = (9, 11)
+        self.led.set_pixel(offset[0] + 0, offset[1] + 0, 1)
+
+        offset = (11, 11)
+        number = bitmap.number(day)
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        offset = (15, 11)
+        number = bitmap.number(day)
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        rlock.release()
+        print "display_date"
+
+        for row in range(16):
+            for col in range(32):
+                screen = self.led.screen
+            print screen[row]
+
+
+if __name__ == "__main__":
+
+    # LEDの表示をスタート
+    led = LedThread()
+    led.start()
+    try:
+        while True:
+            led.display_date(0,1)
+
+    except KeyboardInterrupt:
+        print '\nKeyboard Interrupt'
+
+    led.stop()
+    led.join()
+
+#    GPIO.cleanup()
