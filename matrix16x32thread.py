eae2317c82ba80a7566aefbe661d24da723039f8
ynose_fsd <nose@fsd-jp.com>
2015-03-10 08:49:53 +0900

    Add matrix16x32core.py no thread

--- matrix16x32thread.py
+++ matrix16x32thread.py
@@ -48,83 +48,93 @@ class MatrixLED:
     def screen():
         return self.sereen
 
-    def bits_from_int(self, x):
+    def bits_from_int(x):
         a_bit = x & 1
         b_bit = x & 2
         c_bit = x & 4
         return (a_bit, b_bit, c_bit)
 
-    def set_row(self, row):
+    def set_row(row):
 #        #time.sleep(delay)
-        a_bit, b_bit, c_bit = self.bits_from_int(row)
+        a_bit, b_bit, c_bit = bits_from_int(row)
 #        GPIO.output(a_pin, a_bit)
 #        GPIO.output(b_pin, b_bit)
 #        GPIO.output(c_pin, c_bit)
 #        #time.sleep(delay)
 
-    def set_color_top(self, color):
+    def set_color_top(color):
 #        #time.sleep(delay)
-        red, green, blue = self.bits_from_int(color)
+        red, green, blue = bits_from_int(color)
 #        GPIO.output(red1_pin, red)
 #        GPIO.output(green1_pin, green)
 #        GPIO.output(blue1_pin, blue)
 #        #time.sleep(delay)
      
-    def set_color_bottom(self, color):
+    def set_color_bottom(color):
 #        #time.sleep(delay)
-        red, green, blue = self.bits_from_int(color)
+        red, green, blue = bits_from_int(color)
 #        GPIO.output(red2_pin, red)
 #        GPIO.output(green2_pin, green)
 #        GPIO.output(blue2_pin, blue)
 #        #time.sleep(delay)
 
-    def clock(self):
+    def clock():
 #        GPIO.output(clock_pin, 1)
 #        GPIO.output(clock_pin, 0)
-        return
+        print "clock"
      
-    def latch(self):
+    def latch():
 #        GPIO.output(latch_pin, 1)
 #        GPIO.output(latch_pin, 0)
-        return
+        print "latch"
 
     def set_pixel(self, x, y, color):
         self.screen[y][x] = color
 
     # LEDの表示リフレッシュ
-    def refresh(self):
+    def refresh():
         # 上下８行ずつに分けて出力する
         for row in range(8):
 #            GPIO.output(oe_pin, 1)
-            self.set_color_top(0)
-            self.set_row(row)
+            set_color_top(0)
+            set_row(row)
             #time.sleep(delay)
             for col in range(32):
-                self.set_color_top(self.screen[row][col])
-                self.set_color_bottom(self.screen[row+8][col])
-                self.clock()
+                set_color_top(screen[row][col])
+                set_color_bottom(screen[row+8][col])
+                clock()
             #GPIO.output(oe_pin, 0)
-            self.latch()
+            latch()
 #            GPIO.output(oe_pin, 0)
             time.sleep(delay)
 
-        # Matrixのbitをコンソールに表示
-        print "MatrixLED.refresh"
-        for row in range(len(self.screen)):
-            print self.screen[row]
-
-
-class Display:
+# LED表示スレッド
+rlock = threading.RLock()
+class LedThread(threading.Thread):
     def __init__(self):
-        print "Display.init"
+        print "LedThread.init"
+        threading.Thread.__init__(self)
 
         self.led = MatrixLED()
+        self.running = True
 
-    def refresh(self):
-        self.led.refresh()
+    def run(self):
+        print "LedThread.run"
+
+        while self.running:
+            rlock.acquire()
+
+            self.led.refresh
+#            time.sleep(0.005)
+            rlock.release
+            
+    def stop(self):
+        self.running = False
+        print "stop"
 
     def display_date(self, month, day):
         # 日付mm.ddの表示
+        rlock.acquire()
 
         bitmap = BitmapNumber()
 
@@ -163,6 +173,68 @@ class Display:
             for x in range(len(number[y])):
                 self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
 
+        rlock.release()
+
+        # Matrixのbitをコンソールに表示
+        print "LedThread.display_date"
+        screen = self.led.screen
+        for row in range(len(screen)):
+            print screen[row]
+
+class Display
+    def __init__(self):
+        print "Display.init"
+
+        self.led = MatrixLED()
+
+    def display_date(self, month, day):
+        # 日付mm.ddの表示
+        rlock.acquire()
+
+        bitmap = BitmapNumber()
+
+        left = 0
+        top = 11
+        
+        offset = (left, top)
+        number = bitmap.bit_of_number(month)
+        left += len(number) - 1
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        offset = (left, top)
+        number = bitmap.bit_of_number(month)
+        left += len(number) - 1
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        offset = (left, 15)
+        left += 2
+        self.led.set_pixel(offset[0] + 0, offset[1] + 0, 1)
+
+        offset = (left, top)
+        number = bitmap.bit_of_number(day)
+        left += len(number) - 1
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        offset = (left, top)
+        number = bitmap.bit_of_number(day)
+        left += len(number) - 1
+        for y in range(len(number)):
+            for x in range(len(number[y])):
+                self.led.set_pixel(offset[0] + x, offset[1] + y, number[y][x])
+
+        rlock.release()
+
+        # Matrixのbitをコンソールに表示
+        print "LedThread.display_date"
+        screen = self.led.screen
+        for row in range(len(screen)):
+            print screen[row]
 
 # 数字のビットマップ定義クラス
 class BitmapNumber:
@@ -188,13 +260,15 @@ class BitmapNumber:
 if __name__ == "__main__":
 
     # LEDの表示をスタート
-    display = Display()
+    led = MatrixLED()
     try:
         while True:
-            display.display_date(0, 1)
-            display.refresh()
+            led.display_date(0, 1)
 
     except KeyboardInterrupt:
         print '\nKeyboard Interrupt'
+
+    led.stop()
+    led.join()
 
 #    GPIO.cleanup()
